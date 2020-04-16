c7cda055064dbc9e6df796e36b311d33b78506db
richarddevers <ritchiedev@hotmail.com>
2020-04-16 17:01:55 +0200

    Parse P chapter recursively

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import * as fs from "fs";
-
+import { JSDOM } from "jsdom";
+import {tocToIgnore} from './config';
+import { connect } from "http2";
 // Get file to do
 const scrappedFileTodo: string[] = fs.readdirSync("./scrapped");
 
@@ -7,56 +9,39 @@ const scrappedFileTodo: string[] = fs.readdirSync("./scrapped");
 console.log(`file: ${scrappedFileTodo[0]}`);
 const fileData = fs.readFileSync("./scrapped/" + scrappedFileTodo[0], "utf8");
 
-//////////////////////////////
-// import { parse } from 'fast-html-parser';
-// Use DOM parser
-// const rootDom = parse(fileData)
-
-// // get title
-// const title =  rootDom.querySelector('.page-header__title').text;
-// console.log(`title: ${title}`)
-
-// // get table of content part
-// const toc1 = rootDom.querySelectorAll('.mw-headline');
-// console.log(toc1[0])
-// toc1.forEach((x)=>console.log(x.text))
-//////////////////////////////
-
-//////////////////////////////
-// import { createParser } from 'htmljs-parser';
-// let toto = createParser(fileData)
-// console.log(toto.onText)
-//////////////////////////////
-
-import { JSDOM } from "jsdom";
 
 // Generate DOM & extract window document
 const dom = new JSDOM(fileData);
 const dwd = dom.window.document;
 
 // Get title
-const title = dwd.querySelector(".page-header__title");
+// const title = dwd.querySelector(".page-header__title");
 // console.log(title?.textContent)
 
 // Get Note
+// console.log("======")
+// console.log("NOTE")
+// console.log("======")
 // const note = dwd.querySelector(".box");
 // console.log(note?.textContent)
-// console.log(note?.innerHTML)
+// console.log("======")
 
 // Get TOC (table of content)
+console.log("======")
+console.log("TOC")
+console.log("======")
 const toc = dwd.querySelectorAll(".mw-headline");
+toc.forEach((t)=>console.log(t.textContent))
+console.log("======")
 
-// For each element of table of content, get text & inner html
-// console.log(toc[0])
-// console.log(toc[0].parentElement)
-// console.log(toc[0].parentElement?.nextElementSibling?.textContent)
-// console.log(toc[0].parentElement?.nextElementSibling?.innerHTML)
-// console.log(toc[0].parentElement?.nextElementSibling?.outerHTML)
-////////////////////
+// Get Data from each toc
 let tocResult = {};
 
 for (let t of toc) {
-  if (t.textContent) {
+  if (t.textContent && tocToIgnore.indexOf(t.textContent) === -1) {
+console.log("======")
+// console.log(t.textContent)
+console.log("======")
     const nextElem = t.parentElement?.nextElementSibling;
     if (nextElem) {
         nextElemProxy(t.textContent, nextElem);
@@ -67,13 +52,17 @@ for (let t of toc) {
 console.log("Result")
 console.log(JSON.stringify(tocResult))
 
+
+
+
 function nextElemProxy(tocTitle:string, nextElem: Element){
     if (nextElem.nodeName === "P") {
         console.log("P not implemented yet")
+        // console.log(nextElem.textContent)
         // setP(tocTitle, nextElem);
       } else if (nextElem.nodeName === "DL") {
           console.log("DL not implemented yet")
-        // setDL(tocTitle, nextElem);
+          // setDL(tocTitle, nextElem);
       } else if (nextElem.nodeName === "H3") {
           console.log(`${tocTitle} H3 not implemented yet`)
       } else if (nextElem.nodeName === "DIV") {
@@ -84,29 +73,41 @@ function nextElemProxy(tocTitle:string, nextElem: Element){
 }
 
 function setP(tocTitle: string, nextElem: Element) {
-  setTocResult(tocTitle, nextElem.textContent, nextElem.innerHTML);
+  if(nextElem && nextElem.textContent){
+    const text = getNextContent(nextElem.textContent, nextElem, "P");
+    setTocResult(tocTitle, text);
+  }
 }
+
+function getNextContent(content: string, nextElem: Element, typeToCheck: string){
+  const next = nextElem.nextElementSibling;
+  if(next && next.nodeName === typeToCheck && next.textContent){
+    let content2 = `${content} \n ${next.textContent}`;
+    let result = getNextContent(content2, next, typeToCheck);
+    return result;
+  }
+  return content;
+}
+
 
 function setDL(tocTitle: string, nextElem: Element) {
   const subTitle = nextElem.firstChild?.firstChild?.firstChild?.textContent;
-  setTocSubResult(tocTitle, subTitle,  nextElem.textContent, nextElem.innerHTML);
+  setTocSubResult(tocTitle, subTitle,  nextElem.textContent);
 
   let next = nextElem.nextElementSibling;
   if(next?.nodeName==="DL") setDL(tocTitle, next)
 }
 
-function setTocResult(tocTitle: string, tocText: string | null, TocInnerHTML: string) { // ???
+function setTocResult(tocTitle: string, tocText: string | null | undefined) { // ???
   tocResult[tocTitle] = {
-    textContent: tocText,
-    innerHTML: TocInnerHTML,
+    textContent: tocText
   };
 }
 
-function setTocSubResult(tocTitle: string, subTitle:string | null | undefined, tocText: string | null, TocInnerHTML: string) { // ???
+function setTocSubResult(tocTitle: string, subTitle:string | null | undefined, tocText: string | null) { // ???
   if(!tocResult[tocTitle]) tocResult[tocTitle]=[];
   tocResult[tocTitle].push({
-    subTitle: subTitle,
-    textContent: tocText,
-    innerHTML: TocInnerHTML,
+    title: subTitle,
+    textContent: tocText
   })
 }
